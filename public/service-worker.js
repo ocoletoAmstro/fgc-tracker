30be9428bcf0ebad5e8d8eab263af1c468742542
ocoletoAmstro <ocoleto@amstro.es>
2026-04-14 09:55:01 +0200

    Create service-worker.js

---
+++ public/service-worker.js
@@ -0,0 +1,11 @@
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open("fgc-cache").then((cache) => cache.addAll(["/"]))
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
+});
